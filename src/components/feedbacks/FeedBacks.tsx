e37b57bd3b6c64c6cd3a67b223bd91c2213fb9ce
lipe <felipefenske05@gmail.com>
2025-09-29 15:12:26 -0300

    seção feedbacks, ajustes

---
+++ src/components/feedbacks/FeedBacks.tsx
@@ -0,0 +1,31 @@
+import "./Style.css"
+
+export default function FeedBacks() {
+    return (
+        <section className="feedbacks blur-background" id="feedbacks">
+            <h2 className="title">FeedBacks</h2>
+            <article className="comments">
+                <div className="person">
+                    <h2 className="name">Joao pedro</h2>
+                    <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
+                </div>
+                <div className="person">
+                    <h2 className="name">Joao pedro</h2>
+                    <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
+                </div>
+                <div className="person">
+                    <h2 className="name">Joao pedro</h2>
+                    <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
+                </div>
+                <div className="person">
+                    <h2 className="name">Joao pedro</h2>
+                    <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
+                </div>
+                <div className="person">
+                    <h2 className="name">Joao pedro</h2>
+                    <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
+                </div>
+            </article>
+        </section>
+    )
+}
