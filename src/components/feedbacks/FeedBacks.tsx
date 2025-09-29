0304341b194e612b5717b5cdb328a3527bff33f6
lipe <felipefenske05@gmail.com>
2025-09-29 15:55:24 -0300

    seção de contato

--- src/components/feedbacks/FeedBacks.tsx
+++ src/components/feedbacks/FeedBacks.tsx
@@ -6,23 +6,23 @@ export default function FeedBacks() {
             <h2 className="title">FeedBacks</h2>
             <article className="comments">
                 <div className="person">
-                    <h2 className="name">Joao pedro</h2>
+                    <h3 className="name">User 1</h3>
                     <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
                 </div>
                 <div className="person">
-                    <h2 className="name">Joao pedro</h2>
+                    <h3 className="name">User 2</h3>
                     <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
                 </div>
                 <div className="person">
-                    <h2 className="name">Joao pedro</h2>
+                    <h3 className="name">User 3</h3>
                     <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
                 </div>
                 <div className="person">
-                    <h2 className="name">Joao pedro</h2>
+                    <h3 className="name">User 4</h3>
                     <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
                 </div>
                 <div className="person">
-                    <h2 className="name">Joao pedro</h2>
+                    <h3 className="name">User 5</h3>
                     <h4 className="comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at. Et recusandae obcaecati aut voluptates dignissimos. Perferendis tempora facere expedita pariatur, quia doloremque nam, id enim ducimus laboriosam hic rem.</h4>
                 </div>
             </article>
