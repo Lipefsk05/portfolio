e07b352451d8bb5114e49a1c9649b29dfaa06f6e
lipe <felipefenske05@gmail.com>
2025-10-02 22:24:43 -0300

    eslint fix

--- .eslintcr.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["next/core-web-vitals"],
-  ignorePatterns: ["node_modules/", "src/components/galaxy/"], // ignora o Galaxy
-  rules: {
-    "@typescript-eslint/no-unused-vars": "warn"
-  },
-};
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,3 @@
-import { section } from "framer-motion/client"
 import "./Style.css"
 
 export default function Footer(){
